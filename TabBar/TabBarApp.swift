0cc839db69e59923791b27cf0ea3405521f51c02
Jacob Conacher <jake.conacher@gmail.com>
2022-10-24 18:14:00 -0500

    Initial Commit

--- TabBar/TabBarApp.swift
+++ TabBar/TabBarApp.swift
@@ -2,19 +2,16 @@
 //  TabBarApp.swift
 //  TabBar
 //
-//  Created by Madi Lumsden on 10/24/22.
+//  Created by Jacob Conacher on 10/24/22.
 //
 
 import SwiftUI
 
 @main
 struct TabBarApp: App {
-    let persistenceController = PersistenceController.shared
-
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
