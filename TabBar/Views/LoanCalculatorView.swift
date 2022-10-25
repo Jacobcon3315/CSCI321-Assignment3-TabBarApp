f342cc0de1fc0496c8cb4be863c9c1a9f12974e9
Jacob Conacher <jake.conacher@gmail.com>
2022-10-25 09:32:36 -0500

    Added stepper to Loan Calculator

--- TabBar/Views/LoanCalculatorView.swift
+++ TabBar/Views/LoanCalculatorView.swift
@@ -9,12 +9,28 @@ import SwiftUI
 
 struct LoanCalculatorView: View {
     
+    @State private var principal: String = ""
+    @State private var interest: Double = 0.01
+    
     var body: some View {
-        VStack {
+        VStack (spacing: 20){
             Text("Loan Calculator")
+                .font(.title)
+                .bold()
+                .multilineTextAlignment(.center)
+                .padding()
+            
+            TextField("Enter Principal", text: $principal)
+                .keyboardType(.numbersAndPunctuation)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding(.horizontal)
+            
+            Stepper("Interest", value: $interest, in: 0.01...20.00, step: 0.01)
+                .padding(.horizontal)
+            
+            Spacer()
         }
     }
-    
 }
 
 struct LoanCalculatorView_Previews: PreviewProvider {
