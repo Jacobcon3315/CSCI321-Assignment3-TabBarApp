a26f24fc4035d25aac5421473817e1bd7dcf427c
Jacob Conacher <jake.conacher@gmail.com>
2022-10-25 11:56:12 -0500

    Trying to fix calculation error

--- TabBar/Views/LoanCalculatorView.swift
+++ TabBar/Views/LoanCalculatorView.swift
@@ -9,15 +9,26 @@ import SwiftUI
 
 struct LoanCalculatorView: View {
     
-    @State private var principal: String = ""
-    @State private var interest: Double = 0.01
+    @State private var principal: String = "145000"
+    @State private var interest: Double = 4.65
+    @State private var years: Double = 25
+    @State private var payment: Double = 0.00
+    
+    func compute() {
+        
+        let principalD = Double(principal)
+        let pow1 = pow((1.0 + interest),(years * 12.0))
+        let intDivide = (interest / 100.0) / 12.0
+        
+        payment = (principalD! * intDivide * pow1) / (pow1 - 1.0)
+        
+    }
     
     var body: some View {
         VStack (spacing: 20){
             Text("Loan Calculator")
                 .font(.title)
                 .bold()
-                .multilineTextAlignment(.center)
                 .padding()
             
             TextField("Enter Principal", text: $principal)
@@ -27,6 +38,69 @@ struct LoanCalculatorView: View {
             
             Stepper("Interest", value: $interest, in: 0.01...20.00, step: 0.01)
                 .padding(.horizontal)
+                .bold()
+            
+            HStack {
+                
+                Text("Annual Rate")
+                    .bold()
+                    .padding(.horizontal)
+                    
+                Spacer()
+                
+                Text("\(String(format:"%.2f", interest))%")
+                    .padding(.horizontal)
+                
+            }
+            
+            HStack {
+                
+                Text("Number of Years")
+                    .bold()
+                    .padding(.horizontal)
+                    
+                Spacer()
+                
+                Text("\(String(format:"%.0f", years)) years")
+                    .padding(.horizontal)
+                
+            }
+            
+            HStack {
+                
+                Text("10")
+                    .padding(.horizontal)
+                
+                Slider(value: $years, in: 10...30, step: 5)
+                    .padding(.horizontal)
+                
+                Text("30")
+                    .padding(.horizontal)
+                
+            }
+            
+            Button (action: {
+                compute()
+            }, label: {
+              Text("Calculate")
+                    .padding()
+                    .background(Color.black)
+                    .foregroundColor(Color.white)
+                    .cornerRadius(50)
+            })
+            
+            HStack {
+                
+                Text("Monthly Payment")
+                    .bold()
+                    .padding(.horizontal)
+                    
+                Spacer()
+                
+                Text("$\(String(format:"%.2f", payment))")
+                    .padding(.horizontal)
+                
+            }
             
             Spacer()
         }
